74d42ef6211cbf2ab7663730c0ff82a3a8814b14
tama <tama@ttk1.net>
2020-07-22 00:30:17 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-# with open('LICENSE') as f:
-#     license = f.read()
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
-  name='rcon',
-  version='0.0.0-SNAPSHOT',
-  descriotion='Python implementation of RCON',
-  long_description=readme,
-  author='tama',
-  author_email='tama@ttk1.net',
-  url='あとで',
-  license='license',
-  pakages=find_packages(exclude=('test',))
+    name='rcon',
+    version='0.0.0-SNAPSHOT',
+    descriotion='Python implementation of RCON',
+    long_description=readme,
+    author='tama@ttk1',
+    author_email='tama@ttk1.net',
+    url='https://github.com/ttk1/py-rcon',
+    license=license,
+    pakages=find_packages(exclude=('test',))
 )
