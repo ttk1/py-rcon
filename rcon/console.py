0010730246b1ea808abe5a87ceb19d682d6d85db
tama <tama@ttk1.net>
2020-07-24 03:08:09 +0900

    add test_console

--- rcon/console.py
+++ rcon/console.py
@@ -4,13 +4,13 @@ from rcon.packet import Packet, PacketType
 
 class Console():
     def __init__(self, host, password, port=25575):
-        self.id = 0
-        self.conn = Connection(host, port)
+        self._conn = Connection(host, port)
+        self._id = 0
         self._login(password)
 
     def _get_id(self):
-        self.id += 1
-        return self.id
+        self._id += 1
+        return self._id
 
     def _login(self, password):
         req = Packet(
@@ -18,8 +18,8 @@ class Console():
             type=PacketType.SERVERDATA_AUTH,
             body=password
         )
-        self.conn.send_packet(req)
-        res = self.conn.recv_packet()
+        self._conn.send_packet(req)
+        res = self._conn.recv_packet()
         res.print()
 
     def command(self, command):
@@ -28,9 +28,9 @@ class Console():
             type=PacketType.SERVERDATA_EXECCOMMAND,
             body=command
         )
-        self.conn.send_packet(req)
-        res = self.conn.recv_packet()
+        self._conn.send_packet(req)
+        res = self._conn.recv_packet()
         res.print()
 
     def close(self):
-        self.conn.close()
+        self._conn.close()
---
+++ test/test_console.py
@@ -0,0 +1,113 @@
+from unittest import TestCase
+from unittest.mock import patch, Mock, call
+
+from rcon.console import Console
+from rcon.packet import PacketType
+
+
+class TestPacket(TestCase):
+    @patch('rcon.console.Connection')
+    def setUp(self, Connection):
+        conn = Mock()
+        Connection.return_value = conn
+        self.conn = conn
+        self.console = Console('localhost', 'password')
+
+    @patch('rcon.console.Connection')
+    @patch('rcon.console.Console._login')
+    def test_init(self, _login, Connection):
+        Console('localhost', 'password')
+
+        expected = call('localhost', 25575)
+        actual = Connection.call_args
+        self.assertEqual(expected, actual)
+
+        expected = 1
+        actual = self.console._id
+        self.assertEqual(expected, actual)
+
+        expected = call('password')
+        actual = _login.call_args
+        self.assertEqual(expected, actual)
+
+        # with port specitication
+        Console('localhost', 'password', port=1234)
+
+        expected = call('localhost', 1234)
+        actual = Connection.call_args
+        self.assertEqual(expected, actual)
+
+    def test_get_id(self):
+        self.console._id = 1234
+        res = self.console._get_id()
+
+        expected = 1235
+        actual = res
+        self.assertEqual(expected, actual)
+
+        actual = self.console._id
+        self.assertEqual(expected, actual)
+
+    @patch('rcon.console.Console._get_id')
+    def test_login(self, _get_id):
+        _get_id.return_value = 1234
+        # reset call count of recv_packet
+        self.conn.recv_packet.call_count = 0
+        self.console._login('password')
+        packet = self.conn.send_packet.call_args.args[0]
+
+        # ID
+        expected = 1234
+        actual = packet.id
+        self.assertEqual(expected, actual)
+
+        # Type
+        expected = PacketType.SERVERDATA_AUTH
+        actual = packet.type
+        self.assertEqual(expected, actual)
+
+        # Body
+        expected = 'password'
+        actual = packet.body
+        self.assertEqual(expected, actual)
+
+        # check call count of recv_packet
+        expected = 1
+        actual = self.conn.recv_packet.call_count
+        self.assertEqual(expected, actual)
+
+    @patch('rcon.console.Console._get_id')
+    def test_command(self, _get_id):
+        _get_id.return_value = 1234
+        # reset call count of recv_packet
+        self.conn.recv_packet.call_count = 0
+        self.console.command('say hello')
+        packet = self.conn.send_packet.call_args.args[0]
+
+        # ID
+        expected = 1234
+        actual = packet.id
+        self.assertEqual(expected, actual)
+
+        # Type
+        expected = PacketType.SERVERDATA_EXECCOMMAND
+        actual = packet.type
+        self.assertEqual(expected, actual)
+
+        # Body
+        expected = 'say hello'
+        actual = packet.body
+        self.assertEqual(expected, actual)
+
+        # check call count of recv_packet
+        expected = 1
+        actual = self.conn.recv_packet.call_count
+        self.assertEqual(expected, actual)
+
+    def test_close(self):
+        self.conn.close.call_count = 0
+        self.console.close()
+
+        expected = 1
+        actual = self.conn.close.call_count
+        self.assertEqual(expected, actual)
