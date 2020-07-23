a05a9a0f1c5a77d48c48acf9622b5a8279f1af20
tama <tama@ttk1.net>
2020-07-24 04:51:14 +0900

    typo

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     author_email='tama@ttk1.net',
     url='https://github.com/ttk1/py-rcon',
     license=license,
-    pakages=find_packages(exclude=('test',))
+    packages=find_packages(exclude=('test',))
 )
