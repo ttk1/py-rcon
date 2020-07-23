7540735c9242d73ee12f44233c4c4c175250c456
tama <tama@ttk1.net>
2020-07-23 21:27:43 +0900

    add test_packet

---
+++ test/test_packet.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+from unittest.mock import Mock, patch, call
+
+from rcon.packet import Packet, PacketType
+
+
+class TestPacket(TestCase, Packet):
+    def test_from_bytes(self):
+        packet_data = (
+            # ID = 1
+            b'\x01\x00\x00\x00' +
+            # Type = SERVERDATA_AUTH(3)
+            b'\x03\x00\x00\x00' +
+            # Body
+            b'password'
+            # Empty String
+            b'\x00'
+        )
+        packet = Packet.from_bytes(packet_data)
+
+        # ID
+        expected = 1
+        actual = packet.id
+        self.assertEqual(expected, actual)
+
+        # Type
+        expected = PacketType.SERVERDATA_AUTH
+        actual = packet.type
+        self.assertEqual(expected, actual)
+
+        # Body
+        expected = 'password'
+        actual = packet.body
+        self.assertEqual(expected, actual)
+
+    def test_to_bytes(self):
+        packet = Packet(
+            id=1,
+            type=PacketType.SERVERDATA_AUTH,
+            body='password'
+        )
+
+        expected = (
+            # ID = 1
+            b'\x01\x00\x00\x00' +
+            # Type = SERVERDATA_AUTH(3)
+            b'\x03\x00\x00\x00' +
+            # Body
+            b'password'
+            # Empty String
+            b'\x00'
+        )
+        actual = packet.to_bytes()
+        self.assertEqual(expected, actual)
