882edff7f0cb983eb4e45d80931e2563ec599859
tama <tama@ttk1.net>
2023-12-07 23:15:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='rcon',
-    version='1.2.0',
+    version='1.2.1',
     description='Python implementation of RCON',
     long_description=readme,
     author='tama@ttk1',
